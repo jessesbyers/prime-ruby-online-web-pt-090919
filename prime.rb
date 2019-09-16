e2caf207035003294bc342583f5f483f2fb338a6
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-16 14:32:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 def prime?(integer)
   range = (1..integer).to_a
   results = []
-    range.each_with_index do |n|
-    results << (integer.to_f / n)
-end
-results.keep_if do |value|
-  value % 1 == 0.0
-end
-if results.length == 2
-  return true
-else
-  return false
-end
+  range.each_with_index do |n|
+   results << (integer.to_f / n)
+  end
+  results.keep_if do |value|
+   value % 1 == 0.0
+  end
+  if results.length == 2
+   return true
+  else
+   return false
+  end
 end
